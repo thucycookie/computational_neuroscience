1e720ab74c84215396c9e76ccfba63de8e9d92e3
tug98850 <39741492+tug98850@users.noreply.github.com>
2020-04-11 15:57:39 -0400

    Update tetgen_H.sh

--- tetgen_H.sh
+++ tetgen_H.sh
@@ -8,7 +8,14 @@ unzip tetgen1.4.3.zip
 # Must have Cmake for TETGEN
 cp $HOME/scripts_for_ug/CMakeLists.txt $HOME/tetgen1.4.3/
 
-cd tetgen1.4.3 && mkdir build && cd build && cmake .. && make
+cd tetgen1.4.3 && mkdir build && cd build
+
+# this is to add -fPIC flags to the makefile
+sed -i 's/CXXFLAGS = -O3/CXXFLAGS = -O3 -fPIC/g' ../makefile
+sed -i 's/PREDCXXFLAGS = -O0 -fPI/PREDCXXFLAGS = -O0 -fPI -fPIC/g' makefile 
+
+make
+make tetlib
 
 # very important
 # must cd into UG4 build folder
